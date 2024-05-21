347cfa508ecf410b2b020814b092f1fd62ea33d2
Daniel Gomez <dgomez@linq.com>
2024-05-20 21:22:14 -0500

    Fix style

--- final_project/asyncawaitscript.js
+++ final_project/asyncawaitscript.js
@@ -2,39 +2,27 @@ const axios = require('axios');
 const BASE_URL = "http://localhost:5000";
 
 (async () => {
-
-  const requester = axios.create({
-    baseURL: BASE_URL,
-    timeout: 1000,
-  });
-
   async function getAllBooks() {
-    const response = await requester.get();
-    if (response.status === 200) return response.data;
-    else return 'Could not get books'
+    const response = await axios.get(BASE_URL);
+    return response.data;
   }
+  console.log(await getAllBooks())
 
   async function getBookByISBN(isbn) {
-    const response = await requester.get(`isbn/${isbn}`);
-    if (response.status === 200) return response.data;
-    else return 'Could not get book'
+    const response = await axios.get(`${BASE_URL}/isbn/${isbn}`);
+    return response.data;
   }
+  console.log(await getBookByISBN(1))
 
   async function getBookByAuthor(author) {
-    const response = await requester.get(`author/${author}`);
-    if (response.status === 200) return response.data;
-    else return 'Could not get book'
+    const response = await axios.get(`${BASE_URL}/author/${author}`);
+    return response.data;
   }
+  console.log(await getBookByAuthor("Chinua Achebe"))
 
   async function getBookByTitle(title) {
-    const response = await requester.get(`title/${title}`);
-    if (response.status === 200) return response.data;
-    else return 'Could not get book'
+    const response = await axios.get(`${BASE_URL}/title/${title}`);
+    return response.data;
   }
-
-  // Execute requests
-  console.log(await getAllBooks())
-  console.log(await getBookByISBN(1))
-  console.log(await getBookByAuthor("Chinua Achebe"))
   console.log(await getBookByTitle("Things Fall Apart"))
-})();
+})()
